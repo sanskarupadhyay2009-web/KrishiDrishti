50f0c2e03729065d25a543e27abc0000ef969683
sanskarupadhyay2009-web <sanskarupadhyay.2009@gmail.com>
2026-08-04 08:52:04 +0530

    Refactor main app structure and update home page

--- lib/main.dart
+++ lib/main.dart
@@ -1,35 +1,22 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
-
-import 'home_page.dart';
-import 'scan_page.dart';
-import 'result_page.dart';
-import 'settings_page.dart';
+import 'splash_page.dart';
 
 void main() {
-  runApp(const KrishiDrishtiApp());
+  runApp(const KrishiDrishti());
 }
 
-class KrishiDrishtiApp extends StatelessWidget {
-  const KrishiDrishtiApp({super.key});
+class KrishiDrishti extends StatelessWidget {
+  const KrishiDrishti({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'KrishiDrishti',
+      title: "KrishiDrishti",
       theme: ThemeData(
-        useMaterial3: true,
-        colorSchemeSeed: Colors.green,
-        scaffoldBackgroundColor: const Color(0xffF5F8F3),
-        textTheme: GoogleFonts.poppinsTextTheme(),
+        primarySwatch: Colors.green,
       ),
-      home: const HomePage(),
-      routes: {
-        '/scan': (_) => const ScanPage(),
-        '/result': (_) => const ResultPage(),
-        '/settings': (_) => const SettingsPage(),
-      },
+      home: const SplashPage(),
     );
   }
 }
